be6364bffe271a4a2b09b5e5ae7d482d502233f1
anjanikumarkandula <anjanikumarkandula@gmail.com>
2024-07-19 18:09:49 -0400

    created video model to upload videos and added a page to display uploaded videos

--- myapp/views.py
+++ myapp/views.py
@@ -14,8 +14,9 @@ def events(request):
     if request.method == 'POST':
         form=VideoUploadForm(request.POST, request.FILES)
         if form.is_valid():
+            title = form.cleaned_data['Title']
             videos=form.cleaned_data['video']
             form.save()
-            return render(request, 'video.html',context={'videos':videos})
+            return render(request, 'video.html',context={'Title':title,'videos':videos})
     else:
         return render(request, 'events.html')
