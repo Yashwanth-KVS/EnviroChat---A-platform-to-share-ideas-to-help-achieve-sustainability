b1ca7e75f6b9744dcf2724a334b419018e7ea472
Mohammad Ammar <71547331+MohammadAmmar21@users.noreply.github.com>
2024-07-20 19:00:09 -0400

    profile and dropdown created successfully
    Getting data from Member model, display on dynamic drop down and profile page created successfullyu

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,8 @@ app_name = 'myapp'
 urlpatterns = [
     path('', views.home, name='home'),
     path('aboutus/', views.aboutus, name='aboutus'),
+    path('search/', views.search_list, name="search"),
+    path('search_name/', views.search_name, name="search_name"),
+    path('search_details/<int:id>', views.search_detail , name="search_name"),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,39 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Member
 
 
 # Create your views here.
 
 def home(request):
+    search_list(request)
     return render(request, 'home.html')
 
 
 def aboutus(request):
     return render(request, 'aboutus.html')
+
+
+def search_list(request):
+    return render(request, 'search.html')
+
+
+def search_name(request):
+    query = request.GET.get('search', '')
+    objs = Member.objects.filter(username__startswith=query)
+    payload = []
+    for obj in objs:
+        payload.append({
+            'id': obj.pk,
+            'name': obj.username
+        })
+    return JsonResponse({
+        'status': True,
+        'payload': payload
+    })
+
+
+def search_detail(request, id):
+    mem = get_object_or_404(Member, id=id)
+    print(mem)
+    return render(request, "search-details.html", {"member": mem})
