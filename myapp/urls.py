d61e7b9a1eab89ea3c058ad49fd578934216fa3a
Himadhar786 <mareddyhimadhar786@gmail.com>
2024-07-21 22:05:36 -0400

    Added Logout functionality

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,6 +7,7 @@ app_name = 'myapp'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='myapp/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='myapp/logout.html'), name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('password_reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='myapp/password_reset_done.html'), name='password_reset_done'),
