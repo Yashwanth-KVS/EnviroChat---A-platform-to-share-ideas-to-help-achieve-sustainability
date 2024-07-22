295156f8e357af94f70cc9d5b0bf3840064a0297
Subhram Satyajeet <149985240+CaptainSubhram@users.noreply.github.com>
2024-07-21 23:09:31 -0400

    Unversioned files commit

---
+++ myapp/middleware.py
@@ -0,0 +1,17 @@
+from .models import SiteVisit
+
+
+class VisitCounterMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        # Increment global visit counter
+        # site_visit, created = SiteVisit.objects.get_or_create(id=1)
+        # site_visit.visit_count += 1
+        # site_visit.save()
+        # print(f"Updated global visit count to: {site_visit.visit_count}")
+
+        return response
